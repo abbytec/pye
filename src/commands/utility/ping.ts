09f92cba8dc24534a0d1fbdd70eb284e33696972
Damián Ferreyra <ferreyradamian3755@gmail.com>
2024-10-30 23:40:44 -0300

    Ahora el comando debería responder a la interacción original

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,10 +1,12 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder} from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export default {
-	data: new SlashCommandBuilder()
-		.setName("ping")
-		.setDescription("Pong!"),
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Pong!"),
+    
     async execute(interaction: ChatInputCommandInteraction) {
-      interaction.reply("Pong!");
+        // Enviar la respuesta "Pong!"
+        await interaction.reply("Pong!");
     },
-  };
+};
