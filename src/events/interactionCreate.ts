d5807987986932ff17fe76fff4e763147df87420
Abbytec <gpsmurfs@gmail.com>
2024-11-17 12:08:17 -0300

    interactionCreate buttons

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,29 +1,141 @@
-import { Events, Interaction } from "discord.js";
+import { ButtonInteraction, EmbedBuilder, Events, Interaction, TextChannel } from "discord.js";
 import { ExtendedClient } from "../client.ts";
+import { COLORS, getChannelFromEnv, USERS } from "../utils/constants.ts";
+import { checkQuestLevel, IQuest } from "../utils/quest.ts";
+import { HelperPoint } from "../Models/HelperPoint.ts";
+import { updateMemberReputationRoles } from "../utils/finalwares/updateRepRoles.ts";
 
 export default {
 	name: Events.InteractionCreate,
 	async execute(interaction: Interaction) {
 		// Verifica si la interacción es un comando de texto
-		if (!interaction.isChatInputCommand()) return;
+		if (interaction.isChatInputCommand()) {
+			const command = (interaction.client as ExtendedClient).commands.get(interaction.commandName);
 
-		const command = (interaction.client as ExtendedClient).commands.get(interaction.commandName);
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				if (interaction.replied || interaction.deferred) {
+					await interaction.followUp({ content: "Un error ejecutando este comando!", ephemeral: true });
+				} else {
+					await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+				}
+			}
 			return;
 		}
+		if (interaction.inGuild() && interaction.isButton()) {
+			const customId = interaction.customId;
+			const userId = interaction.user.id;
 
-		// Ejecutar el comando
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: "Un error ejecutando este comando!", ephemeral: true });
-			} else {
-				await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+			// Boton de cerrar el warn cuando el user no tenía md abierto
+			if (customId === "close_warn") return deleteChannel(interaction);
+
+			// Boton de eliminar el mensaje de #puntos
+			if (customId === "cancel-point") return cancelPoint(interaction);
+
+			// Boton de dar puntos en #puntos
+			if (/^\d{17,19}$/.test(customId)) {
+				if (userId === USERS.maby) {
+					await interaction.reply({
+						content:
+							"No vas a dar puntos hoy mi reina <a:JigglerLove:994460658089328710>\nSi ves esto te amo mucho ♥, *No te enojes :c*",
+						ephemeral: true,
+					});
+					return;
+				}
+				return helpPoint(interaction);
 			}
 		}
 	},
 };
+
+// Función para eliminar el canal
+async function deleteChannel(interaction: ButtonInteraction): Promise<void> {
+	try {
+		await interaction.deferUpdate();
+		const channel = interaction.client.channels.resolve(interaction.channelId) as TextChannel | null;
+		if (channel?.isTextBased()) {
+			await channel.delete();
+			console.log(`Canal ${channel.name} eliminado por ${interaction.user.tag}`);
+		}
+	} catch (error) {
+		console.error("Error al eliminar el canal:", error);
+	}
+}
+
+// Función para cancelar el punto (eliminar el mensaje)
+async function cancelPoint(interaction: ButtonInteraction): Promise<void> {
+	try {
+		const message = await interaction.message.fetch();
+		await message.delete();
+		await interaction.deferUpdate();
+	} catch (error) {
+		console.error("Error al eliminar el mensaje:", error);
+	}
+}
+
+// Función para otorgar un punto de ayuda
+async function helpPoint(interaction: ButtonInteraction): Promise<void> {
+	try {
+		// Obtener el miembro que recibirá el punto
+		const member = interaction.guild?.members.cache.get(interaction.customId);
+		if (!member) {
+			await interaction.reply({ content: "Usuario no encontrado.", ephemeral: true });
+			return;
+		}
+
+		// Responder al usuario que ha otorgado el punto
+		await interaction.reply({
+			content: `Le has dado un punto al usuario: \`${member.user.username}\``,
+			ephemeral: true,
+		});
+
+		const embed = EmbedBuilder.from(interaction.message.embeds[0]);
+
+		// Encontrar el campo "PUNTOS OTORGADOS"
+		const fieldIndex = embed.data.fields?.findIndex((field) => field.name === "PUNTOS OTORGADOS");
+
+		if (fieldIndex !== undefined && fieldIndex !== -1 && embed.data.fields) {
+			const existingField = embed.data.fields[fieldIndex];
+			existingField.value += `\n<@${interaction.user.id}> dio 1 punto a <@${member.id}>`;
+			embed.data.fields[fieldIndex] = existingField;
+		} else {
+			embed.addFields({
+				name: "PUNTOS OTORGADOS",
+				value: `<@${interaction.user.id}> dio 1 punto a <@${member.id}>`,
+				inline: false,
+			});
+		}
+		embed.setColor(COLORS.warnOrange);
+		await interaction.message.edit({ embeds: [embed] });
+
+		// Buscar o crear el documento de HelperPoint
+		let user = await HelperPoint.findOneAndUpdate({ _id: interaction.customId }, { $inc: { points: 1 } }, { new: true, upsert: true });
+
+		updateMemberReputationRoles(member, user.points);
+
+		// Enviar notificación en un canal específico
+		const notificationChannel = interaction.client.channels.resolve(getChannelFromEnv("logPuntos")) as TextChannel | null;
+		if (notificationChannel) {
+			await notificationChannel.send(`**${interaction.user.username}** le ha dado un rep al usuario: \`${member.user.username}\``);
+		}
+
+		// Verificar quests
+		checkQuestLevel({ userId: interaction.customId, rep: 1 } as IQuest);
+
+		// Avisarle con un puntito a maby que se le dio puntos al user
+		if (interaction.user.id === USERS.maby) {
+			await interaction.reply({ content: ".", ephemeral: true });
+			return;
+		}
+	} catch (error) {
+		console.error("Error al otorgar punto de ayuda:", error);
+		await interaction.reply({ content: "Hubo un error al otorgar el punto.", ephemeral: true });
+	}
+}
--- src/utils/finalwares/updateRepRoles.ts
+++ src/utils/finalwares/updateRepRoles.ts
@@ -18,22 +18,16 @@ export const updateRepRoles: Finalware = async (postHandleableInteraction, resul
 
 	const { helperPoint, guildMember } = result;
 
-	if (!helperPoint) {
-		console.warn("No se encontró helperPoint en la interacción.");
-		return;
-	}
+	if (!helperPoint) return console.warn("No se encontró helperPoint en la interacción.");
 
-	if (!guildMember) {
-		console.warn("No se encontró member en la interacción.");
-		return;
-	}
+	if (!guildMember) return console.warn("No se encontró member en la interacción.");
 
 	const points = helperPoint.points;
 
 	await updateMemberReputationRoles(guildMember, points);
 };
 
-async function updateMemberReputationRoles(member: GuildMember, points: number): Promise<void> {
+export async function updateMemberReputationRoles(member: GuildMember, points: number): Promise<void> {
 	// Definimos los roles y sus puntos mínimos, ordenados de mayor a menor
 	const rolesWithPoints = Object.entries(ROLES_REP_RANGE).map(([roleName, minPoints]) => ({
 		id: getRoleFromEnv(roleName as keyof typeof ROLES_REP_RANGE),
@@ -43,31 +37,23 @@ async function updateMemberReputationRoles(member: GuildMember, points: number):
 	// Determinamos el rol más alto que el miembro debe tener
 	let newRoleId: string | null = null;
 
-	console.log(rolesWithPoints);
 	for (const role of rolesWithPoints) {
-		if (points >= role.minPoints) {
-			newRoleId = role.id;
-		}
+		if (points >= role.minPoints) newRoleId = role.id;
 	}
-	console.log(newRoleId);
 
 	// Eliminamos todos los roles de reputación actuales
 	const rolesToRemove = rolesWithPoints.map((role) => role.id).filter((roleId) => member.roles.cache.has(roleId));
 
-	if (rolesToRemove.length > 0) {
+	if (rolesToRemove.length > 0)
 		await member.roles
 			.remove(rolesToRemove)
 			.then(() => console.log(`Roles ${rolesToRemove.join(", ")} eliminados de ${member.user.tag}`))
 			.catch((error) => console.error(`Error al eliminar roles de ${member.user.tag}:`, error));
-	}
 
 	// Añadimos el nuevo rol si es necesario
-	if (newRoleId) {
-		if (!member.roles.cache.has(newRoleId)) {
-			await member.roles
-				.add(newRoleId)
-				.then(() => console.log(`Rol ${newRoleId} añadido a ${member.user.tag}`))
-				.catch((error) => console.error(`Error al añadir el rol ${newRoleId} a ${member.user.tag}:`, error));
-		}
-	}
+	if (newRoleId && !member.roles.cache.has(newRoleId))
+		await member.roles
+			.add(newRoleId)
+			.then(() => console.log(`Rol ${newRoleId} añadido a ${member.user.tag}`))
+			.catch((error) => console.error(`Error al añadir el rol ${newRoleId} a ${member.user.tag}:`, error));
 }
