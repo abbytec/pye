3f11e8a02a31e242b23691d0a8c98a5a61a2ffa8
Damián Ferreyra <ferreyradamian3755@gmail.com>
2024-11-27 15:43:08 -0300

    feat:replyInfo con color celeste

---
+++ src/utils/messages/replyInfo.ts
@@ -0,0 +1,47 @@
+import {
+    ActionRowBuilder,
+    AttachmentBuilder,
+    ButtonBuilder,
+    ChatInputCommandInteraction,
+    EmbedBuilder,
+    StringSelectMenuBuilder,
+    TextChannel,
+} from "discord.js";
+import { COLORS } from "../constants.ts";
+
+export async function replyInfo(
+    interaction: ChatInputCommandInteraction,
+    message: string | EmbedBuilder[],
+    author?: string,
+    components?: (ActionRowBuilder<ButtonBuilder> | ActionRowBuilder<StringSelectMenuBuilder>)[],
+    files?: AttachmentBuilder[],
+    content?: string,
+    ephemeral = false
+): Promise<void> {
+    let messageToSend: any = { ephemeral: ephemeral };
+    if (Array.isArray(message)) {
+        messageToSend.embeds = message;
+    } else {
+        messageToSend.embeds = [
+            new EmbedBuilder()
+                .setAuthor({ name: author ?? interaction.user.tag, iconURL: interaction.user.displayAvatarURL() })
+                .setDescription(message)
+                .setColor(COLORS.pyeLightBlue)
+                .setTimestamp(),
+        ];
+    }
+    if (components) messageToSend.components = components;
+    if (files) messageToSend.files = files;
+    if (content) messageToSend.content = content;
+
+    if ((interaction.deferred || interaction.replied) && !components) {
+        if (!ephemeral) {
+            await interaction.deleteReply().catch((e) => null);
+            await (interaction.guild?.channels.resolve(interaction?.channelId) as TextChannel)?.send(messageToSend);
+        } else await interaction.followUp(messageToSend);
+    } else if (components) {
+        await interaction.editReply(messageToSend).catch((e) => null);
+    } else {
+        await interaction.reply(messageToSend);
+    }
+}
