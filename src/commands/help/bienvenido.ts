129103ee0b4c9e8b9bf7ba38dd887dbb1ab51b15
Abbytec <gpsmurfs@gmail.com>
2024-12-03 02:53:06 -0300

    fix: bienvenido deferred

--- src/commands/help/bienvenido.ts
+++ src/commands/help/bienvenido.ts
@@ -4,7 +4,6 @@ import { verifyIsGuild } from "../../utils/middlewares/verifyIsGuild.js";
 import { ExtendedClient } from "../../client.js";
 import { sendWelcomeMessageProcessor } from "../../utils/welcome.js";
 import { verifyCooldown } from "../../utils/middlewares/verifyCooldown.js";
-import { deferInteraction } from "../../utils/middlewares/deferInteraction.js";
 
 export default {
 	group: "📜 - Ayuda",
@@ -12,8 +11,9 @@ export default {
 	data: new SlashCommandBuilder().setName("bienvenido").setDescription("Envía un mensaje de bienvenida a los nuevos usuarios."),
 
 	execute: composeMiddlewares(
-		[verifyIsGuild(process.env.GUILD_ID ?? ""), verifyCooldown("bienvenido", 6e5), deferInteraction()],
+		[verifyIsGuild(process.env.GUILD_ID ?? ""), verifyCooldown("bienvenido", 6e5)],
 		async (interaction: ChatInputCommandInteraction): Promise<void> => {
+			interaction.reply("Dando una calurosa bienvenida");
 			await sendWelcomeMessageProcessor(interaction.client as ExtendedClient, false);
 		}
 	),
