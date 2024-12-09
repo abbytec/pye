61ae7e0c01ab2f97d6d6cef4a7e08aa01a105a5f
Abbytec <gpsmurfs@gmail.com>
2024-12-09 12:39:54 -0300

    console.error

--- src/utils/finalwares/logMessages.ts
+++ src/utils/finalwares/logMessages.ts
@@ -4,7 +4,9 @@ import { Finalware } from "../../types/middleware.js";
 export const logMessages: Finalware = async (postHandleableInteraction, result) => {
 	result.logMessages?.forEach((message) => {
 		if ("content" in message && message.content) {
-			((postHandleableInteraction.member as GuildMember).guild.channels.resolve(message.channel) as TextChannel)?.send(message.content);
+			((postHandleableInteraction.member as GuildMember).guild.channels.resolve(message.channel) as TextChannel)
+				?.send(message.content)
+				.catch(() => console.error("Error al enviar el mensaje de log"));
 		} else if ("user" in message) {
 			let embed = new EmbedBuilder()
 				.setAuthor({ name: message.user.tag, iconURL: message.user.displayAvatarURL() })
@@ -16,10 +18,12 @@ export const logMessages: Finalware = async (postHandleableInteraction, result) 
 						: postHandleableInteraction.guild?.iconURL({ extension: "gif" }) ?? null
 				)
 				.setTimestamp();
-			((postHandleableInteraction.member as GuildMember).guild.channels.resolve(message.channel) as TextChannel).send({
-				embeds: [embed],
-				files: message.attachments,
-			});
+			((postHandleableInteraction.member as GuildMember).guild.channels.resolve(message.channel) as TextChannel)
+				.send({
+					embeds: [embed],
+					files: message.attachments,
+				})
+				.catch(() => console.error("Error al registrar log"));
 		}
 	});
 };
