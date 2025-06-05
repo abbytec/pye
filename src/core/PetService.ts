44e56c8dea3029759021a764be485a90e5f1c970
Abbytec <gpsmurfs@gmail.com>
2025-06-05 19:46:47 -0300

    update PetService + ahora si se te escapa la mascota debes volver a iniciar la ultima quest

--- src/core/PetService.ts
+++ src/core/PetService.ts
@@ -1,13 +1,88 @@
 import { CoreClient } from "./CoreClient.js";
-import { checkPets, checkFood, checkMood, checkShower } from "../commands/items-economy/pet.js";
+import { getRandomNumber } from "../utils/generic.js";
+import { IPetDocument, Pets } from "../Models/Pets.js";
+import { Home } from "../Models/Home.js";
+import { TextChannel } from "discord.js";
+import { getChannelFromEnv } from "../utils/constants.js";
 
 export class PetService {
 	constructor(private readonly client: CoreClient) {}
 
 	startIntervals() {
-		/* 6h30m */ setInterval(() => checkPets(this.client as any), 23400000);
-		/* 8 h  */ setInterval(() => checkFood(), 28800000);
-		/* 4 h  */ setInterval(() => checkMood(), 14400000);
-		/* 5 h  */ setInterval(() => checkShower(), 18000000);
+		/* 6h30m */ setInterval(() => this.checkPets(this.client), 23400000);
+		/* 8 h  */ setInterval(() => this.checkFood(), 28800000);
+		/* 4 h  */ setInterval(() => this.checkMood(), 14400000);
+		/* 5 h  */ setInterval(() => this.checkShower(), 18000000);
+	}
+	private async checkFood() {
+		let arr = await Pets.find().exec();
+		if (!arr.length) return;
+		for (const data of arr) {
+			this.minFeed(data);
+		}
+	}
+
+	private async checkMood() {
+		let arr = await Pets.find().exec();
+		if (!arr.length) return;
+		for (const data of arr) {
+			this.minPlay(data);
+		}
+	}
+
+	private async checkShower() {
+		let arr = await Pets.find().exec();
+		if (!arr.length) return;
+		for (const data of arr) {
+			this.minClean(data);
+		}
+	}
+
+	private async minFeed(petInfo: IPetDocument) {
+		if (!petInfo) return;
+		let total = getRandomNumber(5, 12);
+		if (petInfo.food - total < 0) petInfo.food = 0;
+		else petInfo.food -= total;
+		return await petInfo.save();
+	}
+
+	private async minClean(petInfo: IPetDocument) {
+		if (!petInfo) return;
+		let total = getRandomNumber(5, 12);
+		if (petInfo.shower - total < 0) petInfo.shower = 0;
+		else petInfo.shower -= total;
+		return await petInfo.save();
+	}
+
+	private async minPlay(petInfo: IPetDocument) {
+		if (!petInfo) return;
+		let total = getRandomNumber(5, 12);
+		if (petInfo.mood - total < 0) petInfo.mood = 0;
+		else petInfo.mood -= total;
+		return await petInfo.save();
+	}
+
+	private async checkPets(client: CoreClient) {
+		let arr = await Pets.find().exec();
+		let badge = false;
+		if (!arr.length) return;
+		for (const getData of arr) {
+			if (getData && !getData?.mood && !getData?.food && !getData?.shower) {
+				if (badge) return;
+				badge = true;
+				setTimeout(async () => {
+					let home = await Home.findOne({ id: getData.id }).exec();
+					if (!home) {
+						await getData.deleteOne();
+						return;
+					}
+					await home.updateOne({ text: 0, pet: "none", level: 12, house: { level: 12, color: "Naranja" }, active: false }).exec();
+					await getData.deleteOne();
+					(client.channels.resolve(getChannelFromEnv("casinoPye")) as TextChannel)?.send(
+						`<:petgone:1008539448637665300> - La mascota de <@${getData.id}> se ha escapado debido a que no fue un buen dueño...\nPuedes volver a completar la ultima quest para tener otra mascota, cuidala mejor esta vez!`
+					);
+				}, 7200000);
+			}
+		}
 	}
 }
